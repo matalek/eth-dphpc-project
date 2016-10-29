2dcedea47b4eada3425fefcf27c17855fc06b6bf
Lorenzo <wiismart@hotmail.it>
2016-10-29 12:46:22 +0200

    Modify script with comments and remove logs gen

--- sequential/sequentialScript.py
+++ sequential/sequentialScript.py
@@ -31,36 +31,48 @@ comb_number = int(sys.argv[6])
 cpp_file_path = sys.argv[2]
 generator_path = sys.argv[4]
 
-subprocess.call('make ALGORITH=convex_hull_graham', shell=True)
+#gets the algorithm to use
+algorithm = 'convex_hull_graham'
+
+#build executables
+subprocess.call('make ALGORITHM=' + algorithm, shell=True)
 subprocess.call('g++ -std=c++11 ' + generator_path + ' -o generator', shell=True)
-subprocess.call('mkdir -p logs', shell=True)
 
 exec_time = []
 
-ofile  = open('log_results.csv', "wb")
+#create and open a csv file to store results
+ofile  = open('log_results_' + algorithm + '.csv', "wb")
 writer = csv.writer(ofile, delimiter='	', quotechar='"', quoting=csv.QUOTE_ALL)
-columns_indexes = ['#Points','Exec_Time [us]']
-writer.writerow(columns_indexes)
+writer.writerow(['#Points','Exec_Time [us]'])
 
+#execute tests with different takes for each number of points
 for comb in range(0 , comb_number):
     time_accumulator = 0
+
+    #repeat test the require number of times to get correct avg
     for take in range (0 , rep_number):
         num_of_points = starting_value + step_width*comb
         result = subprocess.check_output('./generator -p ' + str(num_of_points) +
-                                         ' | ./tester | tee logs/log' + str(comb+1) + 'take' + str(take+1) + '.log', shell=True)
+                                         ' | ./tester', shell=True)
 
         #CGAL_result = subprocess.check_output(??)
-        #evaluate correctness
+        #evaluate correctness TODO
+
         curr_time = int(result.split('\n')[0].split(' ')[1])
         time_accumulator += curr_time
+
+    #compute avg of different takes to get a valid result
     avg_time = time_accumulator/rep_number
     exec_time.append(avg_time)
+
+    #write a line in the csv file
     writer.writerow([num_of_points,avg_time])
 
 ofile.close()
+
+#plot results
 x = np.arange(starting_value, (starting_value + step_width*comb_number) , step_width)
 y = np.array(exec_time)
-
 
 plt.title("Number of input points")
 plt.plot(x, y, 'b-', linewidth = 2.0)
