ec921bfc0877f51b82765679e942d2e2df1dcdcc
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2016-11-19 17:31:44 +0100

    fixup! Refactor C++ code to be object-oriented

---
+++ sequential/andrew_algorithm.hh
@@ -0,0 +1,63 @@
+#ifndef ANDREW_ALGORTIHM
+#define ANDREW_ALGORTIHM
+
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+
+#include "../geometric_helpers.hh"
+#include "../convex_hull_sequential_algorithm.hh"
+
+using namespace std;
+
+class AndrewAlgorithm : public ConvexHullSequentialAlgorithm {
+
+public:
+
+	~AndrewAlgorithm() override { }
+
+	// Function which calculates a convex hull of given points set.
+	vector<POINT*> convex_hull(vector<POINT*>& points) override {
+		// Vector of pointer to points to be sorted.
+		vector<POINT*> working_points;
+		vector<POINT*> result_points;
+
+		// Fill up the working_points vector with pointers to points.
+		for (size_t i = 0; i < points.size(); i++) {
+			working_points.push_back(points[i]);
+		}
+
+		// Calculate first lower part of convex hull, connecting most left-bottom
+		// point with most right-top point.
+		unsigned int size_lower_limit = 1;
+		for (POINT* point : working_points) {
+			add_point_to_convex_hull(result_points, point, size_lower_limit);
+		}
+
+		// We do not want already added points to be removed.
+		size_lower_limit = result_points.size();
+		for (int i = working_points.size() - 2; i >= 0; --i) {
+			add_point_to_convex_hull(result_points, working_points[i],
+					size_lower_limit);
+		}
+
+		// Last point has been added two times, so we have to remove it once.
+		result_points.pop_back();
+		return result_points;
+	}
+
+private:
+
+	void add_point_to_convex_hull(vector<POINT*>& result_points, POINT* point,
+			unsigned int size_lower_limit) {
+		while (result_points.size() > size_lower_limit 
+				&& Det((*result_points[result_points.size() - 2]),
+						(*result_points[result_points.size() - 1]), 
+						(*point)) <= 0) {
+			result_points.pop_back();
+		}
+		result_points.push_back(point);
+	}
+};
+
+#endif // ANDREW_ALGORTIHM
---
+++ sequential/graham_algorithm.hh
@@ -0,0 +1,82 @@
+#ifndef GRAHAM_ALGORITHM
+#define GRAHAM_ALGORITHM
+
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+#include "../geometric_helpers.hh"
+
+using namespace std;
+
+class GrahamAlgorithm : public ConvexHullSequentialAlgorithm {
+
+public:
+
+	~GrahamAlgorithm() override { }
+
+	// Function which calculates a convex hull of given points set.
+	vector<POINT*> convex_hull(vector<POINT*>& points) override {
+		// Vector of pointer to points to be sorted.
+		vector<POINT*> working_points;
+		vector<POINT*> result_points;
+
+		// Fill up the working_points vector with pointers to points.
+		POINT* left_bottom = nullptr;
+		for (size_t i = 0; i < points.size(); i++) {
+			if (left_bottom == nullptr || OrderXY(points[i], left_bottom)) {
+				left_bottom = points[i];
+			}
+		}
+
+		for (size_t i = 0; i < points.size(); i++) {
+			if (left_bottom != points[i]) {
+				working_points.push_back(points[i]);
+			}
+		}
+
+		result_points.push_back(left_bottom);
+
+		// Sort points in increasing order with regards to coordinates.
+		sort(working_points.begin(), working_points.end(), AngleComparator(left_bottom));
+
+		// Iterate once over points to create the convex hull.
+		size_t size_lower_limit = 1;
+		for (POINT* point : working_points) {
+			add_point_to_convex_hull(result_points, point, size_lower_limit);
+		}
+
+		return result_points;
+	}
+
+private:
+
+	void add_point_to_convex_hull(vector<POINT*>& result_points, POINT* point,
+			size_t size_lower_limit) {
+		while (result_points.size() > size_lower_limit 
+				&& Det((*result_points[result_points.size() - 2]),
+						(*result_points[result_points.size() - 1]), 
+						(*point)) <= 0) {
+			result_points.pop_back();
+		}
+		result_points.push_back(point);
+	}
+
+	struct AngleComparator {
+		AngleComparator(POINT* point) : point(point) { }
+
+		bool operator()(const POINT* a, const POINT* b) {
+			LL det = Det((*point), (*a), (*b));
+			if (det == 0) {
+				return abs_int(point->x - a->x) + abs_int(point->y - a->y) >
+						abs_int(point->x - b->x) + abs_int(point->y - b->y);
+			}
+			return det > 0;
+		}
+
+		POINT* point;
+	};
+};
+
+#endif // GRAHAM_ALGORITHM
