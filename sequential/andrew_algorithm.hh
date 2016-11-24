a2457480342586a1e99603d5086616d4a8e648d7
Aleksander Matusiak <matusiak.aleksander@gmail.com>
2016-11-24 14:55:08 +0100

    Split convex hull into 2 parts for Andrew's aglorithm

--- sequential/andrew_algorithm.hh
+++ sequential/andrew_algorithm.hh
@@ -46,6 +46,34 @@ public:
 		return result_points;
 	}
 
+	shared_ptr<vector<POINT*> > lower_convex_hull(vector<POINT*>& points) {
+		// Vector of pointer to points to be sorted.
+		shared_ptr<vector<POINT*> > result_points = shared_ptr<vector<POINT*> >(new vector<POINT*>());
+
+		// Calculate first lower part of convex hull, connecting most left-bottom
+		// point with most right-top point.
+		unsigned int size_lower_limit = 1;
+		for (POINT* point : points) {
+			add_point_to_convex_hull(result_points, point, size_lower_limit);
+		}
+
+		return result_points;
+	}
+
+	shared_ptr<vector<POINT*> > upper_convex_hull(vector<POINT*>& points) {
+		// Vector of pointer to points to be sorted.
+		shared_ptr<vector<POINT*> > result_points = shared_ptr<vector<POINT*> >(new vector<POINT*>());
+
+		// Calculate first lower part of convex hull, connecting most left-bottom
+		// point with most right-top point.
+		unsigned int size_lower_limit = 1;
+		for (int i = points.size() - 1; i >= 0; --i) {
+			add_point_to_convex_hull(result_points, points[i], size_lower_limit);
+		}
+
+		return result_points;
+	}
+
 private:
 
 	void add_point_to_convex_hull(shared_ptr<vector<POINT*> > result_points, POINT* point,
