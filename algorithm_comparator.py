d69a60d65d2b634e5a4b120e7b83709fa4df85bc
Lorenzo <wiismart@hotmail.it>
2016-11-26 15:04:06 +0100

    Fix bug in comparator

--- algorithm_comparator.py
+++ algorithm_comparator.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 # -R <number of repetition for each number of points>
 # -a <algorithms to compare in the format <algo1:num_of_threads algo2:num_of_threads algo3:num_of_threads ...>>
 
+
 # Comparator to ensure correctness of result
 def compare_function(cgal , alg):
     # create array with points (remove last char which is \n)
@@ -28,6 +29,7 @@ def compare_function(cgal , alg):
     return True
 
 
+# Check user input correctness
 if int(sys.argv[10]) <= 0 or len(sys.argv) <= 11:
     print 'ERROR in usage, provide int number of repetition for each value (ex. -R 20)'
     exit
@@ -44,14 +46,15 @@ if int(sys.argv[2]) <= 0 or len(sys.argv) <= 3:
     print 'ERROR in usage, provide int number of different combinations (ex. -c 20)'
     exit
 
-args = " ".join(sys.argv[1:11])
 
+# Store parameters passed by user
 CONST_REP_NUMBER = int(sys.argv[10])
 CONST_COORDINATE_RANGE = int(sys.argv[8])
 CONST_STARTING_VALUE = int(sys.argv[6])
 CONST_STEP_WIDTH = int(sys.argv[4])
 CONST_COMB_NUMBER = int(sys.argv[2])
 
+# Initialize a map to contain temporary values
 key_value = {}
 
 #build executables
@@ -62,9 +65,9 @@ subprocess.call('mkdir -p log_files', shell=True)
 subprocess.call('(cd cgal && cmake .)', shell=True)
 subprocess.call('(cd cgal && make)', shell=True)
 
-#Generate CSVs for eveery algorithm tested
-for value in range(0, len(sys.argv) - 12):
-    algorithm = sys.argv[12 + value]
+# Generate empty CSVs for eveery algorithm tested
+for key in range(0, len(sys.argv) - 12):
+    algorithm = sys.argv[12 + key]
     # create and open a csv file to store results
     ofile = open('log_files/log_results_' + algorithm.replace('/', '_').replace(':','_t_') + '.csv', "wb")
     writer = csv.writer(ofile)
@@ -72,17 +75,23 @@ for value in range(0, len(sys.argv) - 12):
     ofile.close()
     key_value[algorithm] = 0
 
+# Start tests
 for num_of_points in range(
         CONST_STARTING_VALUE, CONST_STEP_WIDTH*CONST_COMB_NUMBER + CONST_STARTING_VALUE, CONST_STEP_WIDTH):
 
+    # Print progress information to screen
     print ('\n-----------------------------------------\nBEGINNING STEP: ' +
            str(((num_of_points-CONST_STARTING_VALUE)/CONST_STEP_WIDTH)+1) +
            ', POINTS:' + str(num_of_points) + '\n-----------------------------------------'
            )
 
+    # Repeat each test CONST_REP_NUMBER times to lower effect of randomicity
     for repetition in range(0, CONST_REP_NUMBER):
+
+        # Print progress information to screen
         print '\nGenerationg points for TAKE ' + str(repetition) + '...\n'
 
+        # Generate input points
         subprocess.call(
             'echo "' + str(num_of_points) + ' ' + str(CONST_COORDINATE_RANGE)
             + '" | ./generator/generator > tmp.log', shell=True)
@@ -91,11 +100,13 @@ for num_of_points in range(
         cgal_result = subprocess.check_output(
             'cat tmp.log | ./cgal/cgal_graham_andrew', shell=True)
 
-        for value in range(0,len(sys.argv) - 12):
-            algorithm = sys.argv[12 + value]
+        # Apply every given algorithm to the set of points
+        for key in range(0, len(sys.argv) - 12):
+            algorithm = sys.argv[12 + key]
             algorithm_name = algorithm.split(":")[0]
             concurrency = algorithm.split(":")[1]
 
+            # Print progress information to screen
             print(algorithm + ' REP ' + str(repetition + 1))
 
             # Apply given algorithm. Output of algorithm: time\n resulting_points
@@ -109,19 +120,25 @@ for num_of_points in range(
                 print 'CGAL:\n' + cgal_result
                 sys.exit()
 
+            # Update tmp store
             key_value[algorithm] += int(alg_result.split('\n')[0].split(' ')[1])
 
-    for value in key_value:
-        algorithm = value
+    # Delete tmp file containing the points
+    subprocess.call('rm tmp.log', shell=True)
+
+    # Write a new row in every CSV with this step's result
+    for algorithm in key_value:
         ofile = open('log_files/log_results_' + algorithm.replace('/', '_').replace(':', '_t_') + '.csv', "a")
         writer = csv.writer(ofile)
-        writer.writerow([num_of_points, key_value[value]/CONST_REP_NUMBER])
+        writer.writerow([num_of_points, key_value[algorithm] / CONST_REP_NUMBER])
+        key_value[algorithm] = 0
         ofile.close()
 
+# Print progress information to screen
 print('\n--------------------------------------\n| ----------------------------------- |\n'
       '| |COMPARISON COMPLETED SUCCESSFULLY| |'
       '\n| ----------------------------------- |\n--------------------------------------\n')
-# subprocess.call('./plotter.py -c ' + str(CONST_COMB_NUMBER) + ' -w ' + str(CONST_STEP_WIDTH) + ' -s ' + str(CONST_STARTING_VALUE) +
-#                 ' -a ' + (" ".join(sys.argv[12:])) + ' sequential/convex_hull_graham_coordinate_sort', shell=True)
+
+# Call plotter to plot results
 subprocess.call('./plotter.py -c ' + str(CONST_COMB_NUMBER) + ' -w ' + str(CONST_STEP_WIDTH) + ' -s ' + str(CONST_STARTING_VALUE) +
                 ' -a ' + (" ".join(sys.argv[12:])), shell=True)
