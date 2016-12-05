841debc8e04757438ab16ea880c4cb311164c4fb
Lorenzo <wiismart@hotmail.it>
2016-12-05 21:38:23 +0100

    Fix simpleParallel complexity, moved to right folder

--- simple_parallel/convex_hull_parallel_simple_algorithm.hh
+++ simple_parallel/convex_hull_parallel_simple_algorithm.hh
@@ -4,8 +4,8 @@
 #include <vector>
 #include <memory>
 #include "../geometric_helpers.hh"
-#include "convex_hull_sequential_algorithm.hh"
-#include "convex_hull_parallel_algorithm.hh"
+#include "../algorithm_interfaces/convex_hull_sequential_algorithm.hh"
+#include "../algorithm_interfaces/convex_hull_parallel_algorithm.hh"
 
 using namespace std;
 
@@ -19,13 +19,8 @@ public:
 	// Function which calculates a convex hull of a given points set.
 	shared_ptr<HullWrapper> convex_hull(vector<POINT*>& points) override {
 
-		shared_ptr<vector<POINT*> > upper_points = convex_points(points,1);
-		shared_ptr<vector<POINT*> > lower_points = convex_points(points,0);
-//		for(unsigned int i = (lower_points.get() -> size()) - 2; i > 0 ; i--){
-//			upper_points.get() -> push_back(lower_points -> at(i));
-//		}
-		shared_ptr<VectorConvexHullRepresentation> lower_hull = shared_ptr<VectorConvexHullRepresentation>(new VectorConvexHullRepresentation(lower_points,false));
-		shared_ptr<VectorConvexHullRepresentation> upper_hull = shared_ptr<VectorConvexHullRepresentation>(new VectorConvexHullRepresentation(upper_points,true));
+		shared_ptr<VectorConvexHullRepresentation> lower_hull = shared_ptr<VectorConvexHullRepresentation>(new VectorConvexHullRepresentation(convex_points(points,0),false));
+		shared_ptr<VectorConvexHullRepresentation> upper_hull = shared_ptr<VectorConvexHullRepresentation>(new VectorConvexHullRepresentation(convex_points(points,1),true));
 		shared_ptr<HullWrapper> ret = shared_ptr<HullWrapper>(new HullWrapper(upper_hull, lower_hull));
 		return ret;
 	}
@@ -34,13 +29,16 @@ public:
 	shared_ptr<vector<POINT*> > convex_points(vector<POINT*>& points, bool isUpper){
 		int n = points.size();
 		shared_ptr<ConvexHullRepresentation> partial_results[threads];
+		// Array for the indexing, containing info on how many points each thread has to put in final result
 		int position_array[threads];
+		// Array for the indexing, containing info on which position in final result each thread should start writing at
 		int final_position_array[threads];
-		//Array to store leftmost and rightmost
+		// Array to store leftmost and rightmost points of each separate hull
 		pair<int,int> left_n_right[threads];
 
 		#pragma omp parallel num_threads(threads)
 		{	
+			// Computing the separate convex hulls
 			int id = omp_get_thread_num();
 			// Calculating part of points for the given thread.
 			pair<int, int> range = get_range(n, id);
@@ -56,16 +54,27 @@ public:
 			else{
 				convex_hull_points = sequential_algorithm->lower_convex_hull(working_points);
 			}
+
 			partial_results[id] = shared_ptr<ConvexHullRepresentation>(new R(convex_hull_points, isUpper));
 
 			int leftmost = partial_results[id] -> find_leftmost_point();
 			int rightmost = partial_results[id] -> find_rightmost_point();
+			
+			// Elements used to compute the special case in which leftmost==rightmost.
+			// steepest.first = id in partial results
+			// steepest.second = position of the point we computed the tangent with
+			pair<int,int> steepest_left;
+			pair<int,int> steepest_right;
+			LL max_y_left = 0;
+			LL max_y_right = 0;
 
 			#pragma omp barrier
 
 			//Find rightmost and leftmost for each pair
 			for(int i = 0; i < threads; i++){
+
 				pair<int,int> tangent;
+
 				if(isUpper){
 					if(i < id){
 					//Current element is at right of the alanyzed ch
@@ -74,12 +83,22 @@ public:
 						if(tangent.second < leftmost){
 							leftmost = tangent.second;
 						}
+						if(partial_results[i] -> get_point(tangent.first) -> y > max_y_left){
+							steepest_left.first = i;
+							steepest_left.second = tangent.first;
+							max_y_left = partial_results[i] -> get_point(tangent.first) -> y;
+						}
 					}
 					if(id < i){
 					//Current element is at left of the alanyzed ch
 						tangent = findUpperT(*partial_results[id], *partial_results[i]);
 						if(tangent.first > rightmost){
 							rightmost = tangent.first;
+						}
+						if(partial_results[i] -> get_point(tangent.second) -> y > max_y_right){
+							steepest_right.first = i;
+							steepest_right.second = tangent.second;
+							max_y_right = partial_results[i] -> get_point(tangent.second) -> y;
 						}
 					}
 				}
@@ -91,11 +110,21 @@ public:
 						if(tangent.second > leftmost){
 							leftmost = tangent.second;
 						}
+						if(partial_results[i] -> get_point(tangent.first) -> y < max_y_left){
+							steepest_left.first = i;
+							steepest_left.second = tangent.first;
+							max_y_left = partial_results[i] -> get_point(tangent.first) -> y;
+						}
 					}
 					if(id < i){
 						tangent = findLowerT(*partial_results[id], *partial_results[i]);
 						if(tangent.first < rightmost){
 							rightmost = tangent.first;
+						}
+						if(partial_results[i] -> get_point(tangent.second) -> y < max_y_right){
+							steepest_right.first = i;
+							steepest_right.second = tangent.second;
+							max_y_right = partial_results[i] -> get_point(tangent.second) -> y;
 						}
 					}
 				}
@@ -126,45 +155,28 @@ public:
 			}
 			#pragma omp barrier
 			//Check not single point under line		
-			if(leftmost == rightmost){
+			if(leftmost == rightmost && id!=0 && id!=threads-1){
 
 				POINT* curr_point = partial_results[id] -> get_point(leftmost);
-				for(int i = 0; i < id; i++){
-					for(int j = id+1; j < threads; j++){
-						POINT* left_sider = partial_results[i] -> get_point(left_n_right[i].second);
-						POINT* right_sider = partial_results[j] -> get_point(left_n_right[j].first);
 
-						double y_diff = (right_sider->y - left_sider->y);
-						double x_diff = (right_sider->x - left_sider->x);
-						double m = y_diff/x_diff;
-						//build line and check wether point stands under it
-						double curr_x = (curr_point->x - right_sider->x);
-						double line_y = (m)*(curr_x) + right_sider->y;
+				POINT* left_sider = partial_results[steepest_left.first] -> get_point(steepest_left.second);
+				POINT* right_sider = partial_results[steepest_right.first] -> get_point(steepest_right.second);
+				
+				//build line and check wether point stands under it
+				double y_diff = (right_sider->y - left_sider->y);
+				double x_diff = (right_sider->x - left_sider->x);
+				double m = y_diff/x_diff;
+				double curr_x = (curr_point->x - right_sider->x);
+				double line_y = (m)*(curr_x) + right_sider->y;
 
-						if(isUpper){
-
-							if((long)line_y >= curr_point->y){
-								position_array[id] = 0;
-
-							}
-						}
-						else{
-//if(id==4){
-//printf("\nLEFTNRIGHT id: %d\n", id);
-//left_sider->print();
-//right_sider->print();
-//curr_point->print();
-//printf("Y=%f",line_y);
-//printf("COND%d\n", (long)line_y <= curr_point->y);
-//}
-
-							if((long)line_y <= curr_point->y){
-//if(id==4){
-//printf("KILLER=%f",line_y);
-//}
-								position_array[id] = 0;
-							}
-						}
+				if(isUpper){
+					if((LL)line_y >= curr_point->y){
+						position_array[id] = 0;
+					}
+				}
+				else{
+					if((LL)line_y <= curr_point->y){
+						position_array[id] = 0;
 					}
 				}
 			}
@@ -189,18 +201,14 @@ public:
 			int start_index = final_position_array[id];
 			//start writing from leftmost point
 			int leftmost = left_n_right[id].first;
-			//Check if single point not under line
 
-//printf("\nid: %d, POINTS:\n",id);
 			if(isUpper){
 				for(int i = 0; i < position_array[id]; i++){
-
 					result_points -> at(i + start_index) = partial_results[id] -> get_point(leftmost - i);
 				}
 			}
 			else{
 				for(int i = 0; i < position_array[id]; i++){
-//(partial_results[id] -> get_point(leftmost + i))->print();
 					result_points -> at(i + start_index) = partial_results[id] -> get_point(leftmost + i);
 				}
 			}
