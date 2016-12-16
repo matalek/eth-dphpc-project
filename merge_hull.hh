1c42cbf1973c24469e1a253ef59733f189b1e82a
cervamar <marek.cervak@seznam.cz>
2016-12-16 15:29:03 +0100

    refactoring

--- merge_hull.hh
+++ merge_hull.hh
@@ -10,105 +10,99 @@
 
 using namespace std;
 
-int turnRight(POINT * a, POINT * b, POINT * c) {
-    return DetPointer(a,b,c) < 0;
+int turn_right(POINT *a, POINT *b, POINT *c) {
+    return DetPointer(a, b, c) < 0;
 }
 
-int turnLeft(POINT * a, POINT * b, POINT * c) {
-    return DetPointer(a,b,c) > 0;
+int turn_left(POINT *a, POINT *b, POINT *c) {
+    return DetPointer(a, b, c) > 0;
 }
 
-int lowerTangentA(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
-    POINT * a = hullA.get_point(aIndex);
-    POINT * u = hullA.get_point(hullA.go_counter_clockwise(aIndex));
-    POINT * w = hullA.get_point(hullA.go_clockwise(aIndex));
-    POINT * b = hullB.get_point(bIndex);
-    return turnRight(b, a, w) && !turnLeft(b, a, u);
+int left_lower_tangent(ConvexHullRepresentation &hull, int index, POINT *b) {
+    POINT *a = hull.get_point(index);
+    POINT *u = hull.get_point(hull.go_counter_clockwise(index));
+    POINT *w = hull.get_point(hull.go_clockwise(index));
+    return turn_right(b, a, w) && !turn_left(b, a, u);
 }
 
 // A is always the left hull, B is the right one
-int upperTangentA(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
-    POINT * a = hullA.get_point(aIndex);
-    POINT * u = hullA.get_point(hullA.go_counter_clockwise(aIndex));
-    POINT * w = hullA.get_point(hullA.go_clockwise(aIndex));
-    POINT * b = hullB.get_point(bIndex);
-    return turnLeft(b,a,u) && !turnRight(b,a,w);
+int left_upper_tangent(ConvexHullRepresentation &left_hull, int aIndex, POINT *b) {
+    POINT *a = left_hull.get_point(aIndex);
+    POINT *u = left_hull.get_point(left_hull.go_counter_clockwise(aIndex));
+    POINT *w = left_hull.get_point(left_hull.go_clockwise(aIndex));
+    return turn_left(b, a, u) && !turn_right(b, a, w);
 }
 
-int lowerTangentB(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
-    POINT * a = hullA.get_point(aIndex);
-    POINT * u = hullB.get_point(hullB.go_counter_clockwise(bIndex));
-    POINT * w = hullB.get_point(hullB.go_clockwise(bIndex));
-    POINT * b = hullB.get_point(bIndex);
-    return !turnRight(a, b, w) && turnLeft(a, b, u);
-    //return upperTangentA(hullB, hullA, bIndex, aIndex);
+int lowerTangentB(ConvexHullRepresentation &right_hull, int index, POINT *a) {
+    POINT *u = right_hull.get_point(right_hull.go_counter_clockwise(index));
+    POINT *w = right_hull.get_point(right_hull.go_clockwise(index));
+    POINT *b = right_hull.get_point(index);
+    return !turn_right(a, b, w) && turn_left(a, b, u);
+    //return left_upper_tangent(right_hull, left_hull, index, aIndex);
 }
 
-int upperTangentB(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
-    POINT * a = hullA.get_point(aIndex);
-    POINT * u = hullB.get_point(hullB.go_counter_clockwise(bIndex));
-    POINT * w = hullB.get_point(hullB.go_clockwise(bIndex));
-    POINT * b = hullB.get_point(bIndex);
-    return !turnLeft(a, b, u) && turnRight(a, b, w);
+int right_upper_tangent(ConvexHullRepresentation &right_hull, int index, POINT *a) {
+    POINT *b = right_hull.get_point(index);
+    POINT *u = right_hull.get_point(right_hull.go_counter_clockwise(index));
+    POINT *w = right_hull.get_point(right_hull.go_clockwise(index));
+    return !turn_left(a, b, u) && turn_right(a, b, w);
 }
 
-int findLowerTangentA(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
-    while (!lowerTangentA(hullA, hullB, aIndex, bIndex)) {
-        aIndex = hullA.go_clockwise(aIndex);
+int findLowerTangentA(ConvexHullRepresentation &left_hull, ConvexHullRepresentation &right_hull, int aIndex, int bIndex) {
+    while (!left_lower_tangent(left_hull, aIndex, right_hull.get_point(bIndex))) {
+        aIndex = left_hull.go_clockwise(aIndex);
     }
     return aIndex;
 }
 
-int findLowerTangentB(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
-    while (!lowerTangentB(hullA, hullB, aIndex, bIndex)) {
-        bIndex = hullB.go_counter_clockwise(bIndex);
+int findLowerTangentB(ConvexHullRepresentation &left_hull, ConvexHullRepresentation &right_hull, int aIndex, int bIndex) {
+    while (!lowerTangentB(right_hull, bIndex, left_hull.get_point(aIndex))) {
+        bIndex = right_hull.go_counter_clockwise(bIndex);
     }
     return bIndex;
 }
 
-int findUpperTangentA(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
-    while (!upperTangentA(hullA, hullB, aIndex, bIndex)) {
-        aIndex = hullA.go_counter_clockwise(aIndex);
+int find_left_upper_tangent(ConvexHullRepresentation &hull, int index, POINT *p) {
+    while (!left_upper_tangent(hull, index, p)) {
+        index = hull.go_counter_clockwise(index);
     }
-    return aIndex;
+    return index;
 }
 
-int findUpperTangentB(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
-    while (!upperTangentB(hullA, hullB, aIndex, bIndex)) {
-        bIndex=hullB.go_clockwise(bIndex);
+int find_right_upper_tangent(ConvexHullRepresentation &hull, int index, POINT *p) {
+    while (!right_upper_tangent(hull, index, p)) {
+        index = hull.go_clockwise(index);
     }
-    return bIndex;
+    return index;
 }
 
 //TODO: make it more general
-pair<int,int> findLowerT(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB) {
+pair<int, int> findLowerT(ConvexHullRepresentation &left_hull, ConvexHullRepresentation &right_hull) {
     pair<int, int> returnValues;
-    int aIndex = hullA.find_rightmost_point();
-    int bIndex = hullB.find_leftmost_point();
-    while (!(lowerTangentA(hullA, hullB, aIndex, bIndex) && lowerTangentB(hullA, hullB, aIndex, bIndex))) {
-        aIndex = findLowerTangentA(hullA, hullB, aIndex, bIndex);
-        bIndex = findLowerTangentB(hullA, hullB, aIndex, bIndex);
+    int left_index = left_hull.find_rightmost_point();
+    int right_index = right_hull.find_leftmost_point();
+    while (!(left_lower_tangent(left_hull, left_index, right_hull.get_point(right_index)) &&
+             lowerTangentB(right_hull, right_index, left_hull.get_point(left_index)))) {
+        left_index = find_right_upper_tangent(left_hull, left_index, right_hull.get_point(right_index));
+        right_index = find_left_upper_tangent(right_hull, right_index, left_hull.get_point(left_index));
     }
-    returnValues.first = aIndex;
-    returnValues.second = bIndex;
+    returnValues.first = left_index;
+    returnValues.second = right_index;
     return returnValues;
 }
 
-pair<int,int> findUpperT(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB, int aIndex, int bIndex) {
+pair<int, int> findUpperT(ConvexHullRepresentation &left_hull, ConvexHullRepresentation &right_hull) {
     pair<int, int> returnValues;
-
-    while (!(upperTangentA(hullA, hullB, aIndex, bIndex) && upperTangentB(hullA, hullB, aIndex, bIndex))) {
-        aIndex = findUpperTangentA(hullA, hullB, aIndex, bIndex);
-        bIndex = findUpperTangentB(hullA, hullB, aIndex, bIndex);
+    int left_index = left_hull.find_rightmost_point();
+    int right_index = right_hull.find_leftmost_point();
+    while (!(left_upper_tangent(left_hull, left_index, right_hull.get_point(right_index)) &&
+            right_upper_tangent(right_hull, right_index, left_hull.get_point(left_index)))) {
+        left_index = find_left_upper_tangent(left_hull, left_index, right_hull.get_point(right_index));
+        right_index = find_right_upper_tangent(right_hull, right_index, left_hull.get_point(left_index));
     }
-    returnValues.first = aIndex;
-    returnValues.second = bIndex;
+    returnValues.first = left_index;
+    returnValues.second = right_index;
     return returnValues;
 }
-
-pair<int,int> findUpperT(ConvexHullRepresentation &hullA, ConvexHullRepresentation &hullB) {
-    return findUpperT(hullA, hullB, hullA.find_rightmost_point(), hullB.find_leftmost_point());
-}
-
 
 #endif // MERGE_HULL
