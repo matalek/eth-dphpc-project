9c287223130fae9c65348ad89db15ff4a64a5b8f
Lorenzo <wiismart@hotmail.it>
2017-01-01 12:14:49 +0100

    Beautify plotter

--- scripts/plotter_benchmark.py
+++ scripts/plotter_benchmark.py
@@ -10,29 +10,30 @@ import math
 # Usage ./plotter_benchmark.py
 # -a <algorithms to compare in the format <algo1:num_of_threads algo2:num_of_threads algo3:num_of_threads ...>>
 
-CONST_ALGORITHMS = ['SimpleParallel', 'NaiveParallel', 'HullTree']
+CONST_ALGORITHMS_NAMES = ['SimpleParallel', 'NaiveParallel', 'HullTree']
 CONST_POINTS = 10000000
 CONST_THREADS = [2, 4, 8, 16, 32]
 CONST_SHAPE = 'square'
-colors = ['b', 'r', 'g', 'k', 'y', 'c']
+CONST_COLORS = ['b', 'r', 'g', 'k', 'y', 'c']
+CONST_SOURCE_FILE = ('./log_files/log_files_euler/euler_' + CONST_SHAPE + '/log_results_')
 
 
+# Create the mapping for algorithms {algorithms, execution_times}
 def build_algorithms():
     algorithms = {}
     # Build the map of algorithms
-    for algorithm_name in CONST_ALGORITHMS:
+    for algorithm_name in CONST_ALGORITHMS_NAMES:
 
         for num_of_threads in CONST_THREADS:
             algorithm = Algorithm.Algorithm(algorithm_name + ':' + str(num_of_threads))
-            flag = -1
+            first_row = True
 
-            with open('./log_files/log_files_euler/euler_' + CONST_SHAPE + '/log_results_' + algorithm.name.replace("/", "_").replace(":", "_t_") +
-                              '.csv', 'rb') as f:
+            with open(CONST_SOURCE_FILE + algorithm.name.replace("/", "_").replace(":", "_t_") + '.csv', 'rb') as f:
                 reader = csv.reader(f)
                 for row in reader:
                     # skip first row
-                    if flag == -1:
-                        flag += 1
+                    if first_row:
+                        first_row = False
                         continue
 
                     num_of_input_points = (int(row[0]))
@@ -46,17 +47,16 @@ def build_algorithms():
 
             algorithms[algorithm.name] = algorithm
 
-    # Build Sequential
+    # Build Sequential algorithm
     algorithm = Algorithm.Algorithm('Sequential:1')
-    flag = -1
-
-    with open('./log_files/log_files_euler/euler_' + CONST_SHAPE + '/log_results_' + algorithm.name.replace("/", "_").replace(":", "_t_") +
-                      '.csv', 'rb') as f:
+    first_row = True
+    
+    with open(CONST_SOURCE_FILE + algorithm.name.replace("/", "_").replace(":", "_t_") + '.csv', 'rb') as f:
         reader = csv.reader(f)
         for row in reader:
             # skip first row
-            if flag == -1:
-                flag += 1
+            if first_row:
+                first_row = False
                 continue
 
             num_of_input_points = (int(row[0]))
@@ -73,22 +73,23 @@ def build_algorithms():
     return algorithms
 
 
+# Compute theoretical boundaries
 def calculate_boundary(algorithm_name):
 
     # Uncomment appropriate line depending on input data shape
     if CONST_SHAPE == 'square':
-        expected_size = lambda n : math.log(n, 2)
+        expected_size = lambda n: math.log(n, 2)
     elif CONST_SHAPE == 'disk':
-        expected_size = lambda n : 100 * n**(1/3)
+        expected_size = lambda n: 100 * n**(1/3)
     elif CONST_SHAPE == 'circle':
-        expected_size = lambda n : n
+        expected_size = lambda n: n
 
     if algorithm_name == 'NaiveParallel':
-        overhead = lambda threads : 2 * math.log(threads) * expected_size(CONST_POINTS / threads)
+        overhead = lambda threads: 2 * math.log(threads) * expected_size(CONST_POINTS / threads)
     elif algorithm_name == 'SimpleParallel':
-        overhead = lambda threads : 2 * threads * math.log(CONST_POINTS / threads, 2)**2
+        overhead = lambda threads: 2 * threads * math.log(CONST_POINTS / threads, 2)**2
     elif algorithm_name == 'HullTree':
-        overhead = lambda threads : 10 * math.log(threads, 2) + math.log(CONST_POINTS / threads, 2)
+        overhead = lambda threads: 10 * math.log(threads, 2) + math.log(CONST_POINTS / threads, 2)
 
     boundary = lambda threads : CONST_POINTS / (CONST_POINTS / threads + overhead(threads))
 
@@ -127,7 +128,7 @@ def plot_execution_time(algorithms):
         else:
             my_label = algorithm_name.replace("_", " ").replace(":", " ") + ' threads'
 
-        plt.loglog(num_of_input_points, mean_execution_time, colors[count] + 'o--', label=my_label)
+        plt.loglog(num_of_input_points, mean_execution_time, CONST_COLORS[count] + 'o--', label=my_label)
         # plt.errorbar(num_of_input_points, mean_execution_time, stdv, ecolor=colors[count], fmt='|')
         count += 1
 
@@ -177,7 +178,7 @@ def plot_speedup(algorithms, sequential_algorithm):
 
             my_label = algorithm_name.replace("_", " ").replace(":", " ") + ' threads'
 
-            plt.semilogx(num_of_input_points, speedup, colors[count] + 'o--', label=my_label)
+            plt.semilogx(num_of_input_points, speedup, CONST_COLORS[count] + 'o--', label=my_label)
             count += 1
 
     plt.legend(loc=3)
@@ -199,7 +200,7 @@ def plot_execution_time_fixed_points(algorithms, sequential_algorithm):
     plt.xlabel('Number of threads')
 
     count = 0
-    for algorithm_name in CONST_ALGORITHMS:
+    for algorithm_name in CONST_ALGORITHMS_NAMES:
 
         mean_execution_time = []
         stdv = []
@@ -211,7 +212,7 @@ def plot_execution_time_fixed_points(algorithms, sequential_algorithm):
             mean_execution_time.append(float(np.mean(measured_execution_time) / (10 ** 6)))
             stdv.append(float(np.std(measured_execution_time) / (10 ** 6)))
 
-        plt.plot(CONST_THREADS, mean_execution_time, colors[count] + 'o--', label=my_label)
+        plt.plot(CONST_THREADS, mean_execution_time, CONST_COLORS[count] + 'o--', label=my_label)
         count += 1
 
     # Sequential
@@ -223,7 +224,7 @@ def plot_execution_time_fixed_points(algorithms, sequential_algorithm):
         mean_execution_time.append(mean_sequential_exec_time)
         stdv.append(sequential_stdv)
 
-    plt.plot(CONST_THREADS, mean_execution_time, colors[count] + 'o--', label=my_label)
+    plt.plot(CONST_THREADS, mean_execution_time, CONST_COLORS[count] + 'o--', label=my_label)
     plt.xticks(CONST_THREADS)
     plt.grid(True)
     plt.legend(loc=1)
@@ -242,7 +243,7 @@ def plot_speedup_fixed_points(algorithms, sequential_algorithm):
     plt.xlabel('Number of threads')
 
     count = 0
-    for algorithm_name in CONST_ALGORITHMS:
+    for algorithm_name in CONST_ALGORITHMS_NAMES:
 
         mean_execution_time = []
         stdv = []
@@ -254,7 +255,7 @@ def plot_speedup_fixed_points(algorithms, sequential_algorithm):
             mean_execution_time.append(mean_sequential_exec_time / float(np.mean(measured_execution_time) / (10 ** 6)))
             stdv.append(float(np.std(measured_execution_time) / (10 ** 6)))
 
-        plt.plot(CONST_THREADS, mean_execution_time, colors[count] + 'o--', label=my_label)
+        plt.plot(CONST_THREADS, mean_execution_time, CONST_COLORS[count] + 'o--', label=my_label)
         count += 1
 
     plt.xticks(CONST_THREADS)
@@ -274,7 +275,7 @@ def plot_theoretical_boundaries(algorithms, sequential_algorithm):
     plt.ylabel('Speedup')
     plt.xlabel('Number of threads')
     count = 0
-    for algorithm_name in CONST_ALGORITHMS:
+    for algorithm_name in CONST_ALGORITHMS_NAMES:
         mean_execution_time = []
         stdv = []
         my_label = algorithm_name
@@ -341,25 +342,25 @@ def box_plots(algorithms):
 
 
 # Map of algorithms. The map will be {algorithm_name, Algorithm}
-algorithms = build_algorithms()
+algorithms_map = build_algorithms()
 
 # Sequential algorithm
-seq_algorithm = algorithms['Sequential:1']
+seq_algorithm = algorithms_map['Sequential:1']
 
 # Execution Time plotter
-plot_execution_time(algorithms)
+plot_execution_time(algorithms_map)
 
 # Speedup plotter
-plot_speedup(algorithms, seq_algorithm)
+plot_speedup(algorithms_map, seq_algorithm)
 
 # Execution Time plotter threads on x axis
-plot_execution_time_fixed_points(algorithms, seq_algorithm)
+plot_execution_time_fixed_points(algorithms_map, seq_algorithm)
 
 # Speedup plotter threads on x axis
-plot_speedup_fixed_points(algorithms, seq_algorithm)
+plot_speedup_fixed_points(algorithms_map, seq_algorithm)
 
 # Plot Speedup x numof threads with theoretical boundaries
-plot_theoretical_boundaries(algorithms, seq_algorithm)
+plot_theoretical_boundaries(algorithms_map, seq_algorithm)
 
 # BoxPlots for execution time
-box_plots(algorithms)
+box_plots(algorithms_map)
